36e0dab03c4c7b61cf941ca91395cf0c02e01f7b
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-08-14 23:39:10 +0530

    Update store.js

--- store.js
+++ store.js
@@ -9,3 +9,12 @@ const schema = {
     type: 'number',
     default: 200
   },
+ captureImages: {
+    type: 'boolean',
+    default: true
+  },
+  blacklistApps: {
+    type: 'array',
+    default: [] // e.g., ['KeePass', '1Password']
+  }
+};
