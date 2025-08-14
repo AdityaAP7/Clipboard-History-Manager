8ae224ad61f33a0e056915ba09351cdf56511071
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-08-14 23:39:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import crypto from 'crypto';
 import { store, addEntry } from './store.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
